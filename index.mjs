f1e20b8f9ee7ca8a88b8e00d3a044d7f40a5216e
Thomas Steiner <tomac@google.com>
2022-04-11 18:17:38 +0200

    Web app

--- index.mjs
+++ index.mjs
@@ -3,13 +3,21 @@ import fetch from 'node-fetch';
 import { writeFile } from 'fs/promises';
 import captureWebsite from 'capture-website';
 import filenamifyUrl from 'filenamify-url';
+import ogp from 'ogp-parser';
 
 import limit from './utils.mjs';
+import fuguSVG  from './fugu.svg.mjs';
+import style from './style.css.mjs';
 
 const SPREADSHEET_URL =
   'https://sheets.googleapis.com/v4/spreadsheets/1S_Apr0HavFCO7H9hKcRjIUrgoT7MFRg4uBm7aWSoaYo/values/Sheet2?key=AIzaSyCkROWBarEOJ9hQJggyrlUFulOFA4h6AW0&alt=json';
 
-(async () => {
+const SCREENSHOT_OPTIONS = {
+  width: 1280,
+  height: 800,
+};
+
+const createRawData = async () => {
   const response = await fetch(SPREADSHEET_URL);
   const json = await response.json();
   const values = json.values;
@@ -18,9 +26,13 @@ const SPREADSHEET_URL =
       timestamp: new Date(row[0]),
       appURL: row[1],
       sourceURL: row[2] || null,
-      usedAPIs: row[3]
-        .split(', ')
-        .map((api) => api.replace(/\(https.*\)/g, '').trim()),
+      usedAPIs: row[3].split(', ').map((api) => {
+        return {
+          name: api.replace(/\(https.*\)/g, '').trim(),
+          url: api.replace(/.*?\((https.*)\)/g, '$1'),
+        };
+      }),
+      screenshot: `${filenamifyUrl(row[1])}.png`,
     };
   });
   await writeFile(
@@ -28,20 +40,199 @@ const SPREADSHEET_URL =
     JSON.stringify(data, null, 2),
   );
   console.log('Successfully wrote `data.json`.');
+  return data;
+};
 
-  const options = {
-    width: 1280,
-    height: 800,
-  };
-  const items = data.map((item) => [item.appURL, filenamifyUrl(item.appURL)]);
-  const tasks = items.map(([url, filename]) => {
+const createScreenshots = async (data) => {
+  const items = data.map((item) => [item.appURL, item.screenshot]);
+  const tasks = items.map(([url, filename], i) => {
+    data[i].screenshot = filename;
     return () =>
       captureWebsite.file(
         url,
-        path.resolve('data', `${filename}.png`),
-        options,
+        path.resolve('data', filename),
+        SCREENSHOT_OPTIONS,
       );
   });
   await limit(tasks, 5);
   console.log('Successfully wrote screenshots.');
+};
+
+const createHTML = async (data) => {
+  await Promise.all(
+    data.map(async (item) => {
+      return ogp(item.appURL, { skipOembed: true }).then(
+        (meta) => (item.meta = meta),
+      );
+    }),
+  );
+  const availableAPIs = new Set();
+
+  const header = `
+      <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Fugu API Showcase</title>
+            <link rel="icon" href="${fuguSVG}" />
+            ${style}
+          </head>
+          <body>
+            <h1>Fugu API Showcase</h1>`;
+
+  const form = `
+      <form>
+        <label>
+          Filter used APIs:
+          <input list="available-apis" id="search" type="search" />
+        </label>
+      </form>`;
+
+  const div = `
+      <div class=container>
+        ${data
+          .map((item) => {
+            const title =
+              item.meta.ogp?.['og:title'] ||
+              item.meta.seo?.title ||
+              item.meta.seo?.['og:title'] ||
+              item.meta.seo?.['twitter:title'] ||
+              item.meta.title ||
+              new URL(item.appURL).hostname;
+            const description =
+              item.meta.ogp?.['og:description'] ||
+              item.meta.seo?.description ||
+              item.meta.seo?.['og:description'] ||
+              item.meta.seo?.['twitter:description'] ||
+              item.meta.description ||
+              '';
+            const classes = [];
+            item.usedAPIs.forEach((usedAPI) => {
+              availableAPIs.add(usedAPI.name);
+              classes.push(
+                usedAPI.name.toLowerCase().replace(/[^a-z0-9]/gi, '-').replace(/-+/g, '-').replace(/-*$/g, '')
+              );
+            });
+            return `
+            <article class="${classes.join(' ')}">
+              <h2><a target="_blank" rel="noopener" href="${
+                item.appURL
+              }">${title}</a></h2>
+              <figure>
+                <a target="_blank" rel="noopener" href="${item.appURL}">
+                  <img src="${item.screenshot}" width="${
+              SCREENSHOT_OPTIONS.width
+            }" height="${
+              SCREENSHOT_OPTIONS.height
+            }" alt="Screenshot of ${title}" />
+                </a>
+                ${
+                  description
+                    ? `<figcaption class="description">${description}</figcaption>`
+                    : ''
+                }
+              </figure>
+              ${
+                item.sourceURL
+                  ? `<span class="source"><a target="_blank" rel="noopener" href="${item.sourceURL}">Source</a></span>`
+                  : ''
+              }
+              <span class="launch"><a target="_blank" rel="noopener" href="${
+                item.appURL
+              }">Launch</a></span>
+              <p>Used APIs:</p>
+              <ul>${item.usedAPIs
+                .map(
+                  (usedAPI) =>
+                    `<li><a target="_blank" rel="noopener" href="${usedAPI.url}">${usedAPI.name}</a></li>`,
+                )
+                .join('')}</ul>
+            </article>
+          `;
+          })
+          .join('')}
+      </div>
+      <script>
+        const articles = document.querySelectorAll('article');
+        const options = document.querySelectorAll('option');
+        const input = document.querySelector('input');
+        const form = document.querySelector('form');
+
+        const slugify = (string) => {
+          return string.toLowerCase().replace(/[^a-z0-9]/gi, '-').replace(/-+/g, '-').replace(/-*$/g, '');
+        };
+
+        const availableAPIs = [${Array.from(availableAPIs.keys())
+          .sort()
+          .map((api) => `'${api}'`)
+          .join(', ')
+          .toLowerCase()}].map(slugify);
+
+        form.addEventListener('submit', (e) => {
+          e.preventDefault();
+        });
+
+        options.forEach((option) => {
+          option.addEventListener('click', (e) => {
+            const value = slugify(option.value);
+            input.value = value;
+            input.dispatchEvent(new Event('input'));
+          });
+        });
+
+        input.addEventListener('input', () => {
+          const value = slugify(input.value);
+          const url = new URL(window.location);
+          if (value && availableAPIs.includes(value)) {
+            url.searchParams.set('api', value);
+            window.history.pushState({}, '', url);
+            articles.forEach(article => {
+              article.style.display = 'none';
+              if (article.classList.contains(value)) {
+                article.style.display = 'block';
+              }
+            });
+          } else {
+            articles.forEach(article => {
+              article.style.display = 'block';
+            });
+            url.searchParams.delete('api');
+            window.history.pushState({}, '', url);
+          }
+        });
+
+        window.addEventListener('load', () => {
+          const url = new URL(window.location);
+          const api = url.searchParams.get('api');
+          if (api && availableAPIs.includes(api)) {
+            input.value = api;
+          } else {
+            url.searchParams.delete('api');
+            window.history.pushState({}, '', url);
+          }
+          input.dispatchEvent(new Event('input'));
+        });
+      </script>`;
+
+  const datalist = `
+    <datalist id="available-apis">${Array.from(availableAPIs)
+      .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+      .map(
+        (availableAPI) =>
+          `<option value="${availableAPI}">${availableAPI}</option>`,
+      )
+      .join('')}</datalist>`;
+
+  const footer = `</body></html>`;
+
+  const html = `${header}${form}${datalist}${div}${footer}`;
+  await writeFile(path.resolve('data', 'index.html'), html);
+  console.log('Successfully wrote `index.html`.');
+};
+
+(async () => {
+  const data = await createRawData();
+  await createScreenshots(data);
+  await createHTML(data);
 })();
---
+++ style.css.mjs
@@ -0,0 +1,116 @@
+export default `
+<style>
+  :root {
+    color-scheme: dark light;
+    font-family: system-ui, sans-serif;
+
+    --background-color: #eee;
+    --foreground-color: #000;
+    --accent-color: deeppink;
+  }
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --foreground-color: #fff;
+      --background-color: #222;
+    }
+  }
+  h1 {
+    color: var(--accent-color);
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    filter: none;
+  }
+  img:hover {
+    filter: contrast(1.2);
+  }
+  input {
+    font-size: inherit;
+    width: 30ch;
+  }
+  article {
+    display: none;
+    background-color: var(--background-color);
+    border-radius: 0.5rem;
+    border: solid .25rem var(--background-color);
+  }
+  article > h2 {
+    background-color: var(--background-color);
+    color: var(--foreground-color);
+    margin: 0;
+    padding: 1rem;
+  }
+  h2 > a {
+    text-decoration: none;
+    color: inherit;
+  }
+  figure {
+    margin: 0;
+  }
+  figcaption {
+    padding: 1rem;
+  }
+  a {
+    color: var(--accent-color);
+  }
+  p {
+    padding-inline: 1rem;
+  }
+  ul {
+    padding-inline: 1rem;
+  }
+  li,
+  option {
+    font-size: 0.9rem;
+    display: inline-block;
+    color: #fff;
+    background-color: blue;
+    border-radius: 0.25rem;
+    padding: 0.25rem;
+    margin-inline-end: 0.5rem;
+    margin-block-end: 0.5rem;
+    cursor: pointer;
+  }
+  li > a {
+    color: inherit;
+    text-decoration: none;
+  }
+  datalist {
+    margin-block-start: 1rem;
+    display: inline-block;
+  }
+  .source,
+  .launch {
+    padding-block-start: 0.5rem;
+    padding-inline: 1rem;
+    display: block;
+  }
+  .source::before,
+  .launch::before {
+    content: '';
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    margin-inline-end: 1ex;
+    vertical-align: middle;
+  }
+  .source::before {
+    background-image: url("data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg'%0Aaria-label='GitHub' role='img'%0AviewBox='0 0 512 512'%3E%3Crect%0Awidth='512' height='512'%0Arx='15%25'%0Afill='%23181717'/%3E%3Cpath fill='%23fff' d='M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-44c-71 16-86-34-86-34-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z'/%3E%3C/svg%3E");
+  }
+  .launch::before {
+    filter: none;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 -256 1850 1850'%3E%3Cpath d='M1408 608V288q0-119-84.5-203.5T1120 0H288Q169 0 84.5 84.5T0 288v832q0 119 84.5 203.5T288 1408h704q14 0 23-9t9-23v-64q0-14-9-23t-23-9H288q-66 0-113-47t-47-113V288q0-66 47-113t113-47h832q66 0 113 47t47 113v320q0 14 9 23t23 9h64q14 0 23-9t9-23zm384 864V960q0-26-19-45t-45-19q-26 0-45 19l-176 176-652-652q-10-10-23-10t-23 10L695 553q-10 10-10 23t10 23l652 652-176 176q-19 19-19 45t19 45q19 19 45 19h512q26 0 45-19t19-45z' fill='currentColor' transform='matrix(1 0 0 -1 30.4 1427)'/%3E%3C/svg%3E");
+  }
+  @media (prefers-color-scheme: dark) {
+    .launch::before {
+      filter: invert(1);
+    }
+  }
+  .container {
+    margin-block-start: 1rem;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(30ch, 1fr));
+}
+</style>`;
