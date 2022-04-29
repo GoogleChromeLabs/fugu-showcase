9640fa3df20a34a46080ddb678ff1cf651d9faaf
Thomas Steiner <tomac@google.com>
2022-04-29 09:43:34 +0200

    Add license header to sw.js

--- data/sw.js
+++ data/sw.js
@@ -1,6 +1,18 @@
 
-  const APP_SHELL_CACHE = 'app-shell-4146314848160675';
-  const MEDIA_CACHE = 'media-4146314848160675';
+  /**
+   * Copyright 2022 Google Inc. All Rights Reserved.
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *     http://www.apache.org/licenses/LICENSE-2.0
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   */
+  const APP_SHELL_CACHE = 'app-shell-5280343005812906';
+  const MEDIA_CACHE = 'media-5280343005812906';
 
   const APP_SHELL_FILES = [
     './',
