75ba9ae8d3211fab962b0213f71362ef190be5f4
Thomas Steiner <tomac@google.com>
2023-09-25 09:12:26 +0200

    New release

--- data/sw.js
+++ data/sw.js
@@ -11,8 +11,8 @@
    * See the License for the specific language governing permissions and
    * limitations under the License.
    */
-  const APP_SHELL_CACHE = 'app-shell-3330949125046625';
-  const MEDIA_CACHE = 'media-3330949125046625';
+  const APP_SHELL_CACHE = 'app-shell-8026836601833225';
+  const MEDIA_CACHE = 'media-8026836601833225';
 
   const APP_SHELL_FILES = [
     './',
